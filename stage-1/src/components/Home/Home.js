edeb0d3ce2e261b159868238729c22783fe3b152
Ethan Sanders <ethanedu5@gmail.com>
2018-10-23 14:31:08 -0500

    Finished assignment

--- stage-1/src/components/Home/Home.js
+++ stage-1/src/components/Home/Home.js
@@ -1,6 +1,7 @@
+import React from "react"
 import './Home.css';
 
-class Home extends Component {
+class Home extends React.Component {
     render() {
         return (
             <div className="home-landing"></div>
